c776e8725fed72bccedcf6e9399b67acd12f51d4
Björn Domars <bdomars@walkbase.com>
2017-07-04 15:40:30 +0300

    Regression test with actual data

--- tests.py
+++ tests.py
@@ -52,7 +52,18 @@ class MergeTests(unittest.TestCase):
         self.assertEqual(merged_ranges, [(None, None)])
 
     def test_merge_ranges_regression_1(self):
-        list_of_ranges = [(None, datetime(1990, 2, 10)), (datetime(1990, 2, 15), None), (datetime(1990, 2, 10), datetime(1990, 2, 20))]
+        list_of_ranges = [
+            (None, datetime(2016, 3, 25)),
+            (datetime(2016, 12, 19), datetime(2016, 12, 26)),
+            (datetime(2016, 12, 8), datetime(2016, 12, 19)),
+            (datetime(2016, 12, 26), datetime(2017, 1, 2)),
+            (datetime(2017, 1, 2), datetime(2017, 4, 14)),
+            (datetime(2016, 3, 28), datetime(2016, 11, 26)),
+            (datetime(2016, 3, 25), datetime(2016, 3, 28)),
+            (datetime(2016, 11, 26), datetime(2016, 12, 8)),
+            (datetime(2017, 1, 1), None)
+        ]
+
         merged_ranges = merge_ranges(list_of_ranges)
         self.assertEqual(merged_ranges, [(None, None)])
 
