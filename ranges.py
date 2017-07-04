398ad35b3dc29a3764b618753bccac5400efd04e
Björn Domars <bdomars@walkbase.com>
2017-07-04 15:44:10 +0300

    Seems to fix regression 1

--- ranges.py
+++ ranges.py
@@ -25,9 +25,13 @@ class Range(object):
         return False
 
     def __lt__(self, other):
+        if self.start is not None and self.end is None:
+            return False
+
         if other.start is not None and (self.start <= other.start or self.start is None):
             if other.end is None or (self.end < other.end or self.end is not None):
                 return True
+
         return False
 
     def __repr__(self):
