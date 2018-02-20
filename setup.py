05a964452e3a6689437579ff2424d490502db6fd
Björn Domars <bdomars@walkbase.com>
2018-02-20 11:57:34 +0200

    Add setup()

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='range_utils',
+      version='0.1',
+      description='Some basic stuff for working with date ranges',
+      url='http://github.com/bdomars/range_utils',
+      author='Björn Domars',
+      author_email='bdomars@abo.fi',
+      license='MIT',
+      packages=['range_utils'],
+      zip_safe=False)
